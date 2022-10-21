8aabb9db919787bf6a52eaef9983894bf20cb3f1
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-10-21 13:40:43 -0400

    created useEffect function, passed Character their props and created the Character component returning a div

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import Character from "./components/Character";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-const [characters, setCharacters] = useState();
+const [characters, setCharacters] = useState([]);
 
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        console.log(res.data);
+      }).catch(err => console.error(err));
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characters={characters} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -2,3 +2,8 @@ import React from 'react';
 
 
 // Write your Character component here
+
+
+export default function Character(props) {
+    return (<div></div>)
+}
